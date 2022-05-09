3effe98fefbc1350d7bb4ccdde87424b5d15bf5b
pk409678 <96690266+pk409678@users.noreply.github.com>
2022-05-09 12:54:54 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+
 
 function App() {
+
+  let  [count, uptValue] = useState(0);
+  const valChange = () => {
+     uptValue(count +1);
+  }
+  const valueChange = () => {
+    uptValue(count -1)
+
+ }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container1'>
+    <h1>Counter App</h1>
+    <br></br>
+        <h1 id='num'>{count}</h1>
+        <button onClick={valueChange}   disabled = {count === 0 }>Decrement</button>
+        <button  style={{backgroundColor:'green'}} onClick={valChange}>Increment</button>
+        <br></br>
+        <button style={{backgroundColor:'red',marginTop: '20px'}} onClick={()=>{uptValue(0)}}  disabled = {count === 0 }>Reset</button>
+      </div>
+    </>
   );
 }
 
